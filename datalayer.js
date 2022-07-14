0791171d513f906925f131623db18c04c01e225d
BjornarPaTibe <109271236+BjornarPaTibe@users.noreply.github.com>
2022-07-14 11:16:50 +0200

    Add files via upload

---
+++ datalayer.js
@@ -0,0 +1,14 @@
+window.dataLayer = window.dataLayer || []
+window.dataLayer.push({
+  //Get values from database
+  'produktnavn':'Dethleffs Trend T 7017',
+  'pris': 839000,
+  'salgstype':'brukt',
+  'kilometeravstand': '12000km',
+  'girkasse':'Manuell',
+  'hestekrefter':149,
+  'antall_seter':4,
+  'årsmodell':'2017',
+  'totalvekt': '3850kg',
+  'egenvekt':'3043kg'
+});
