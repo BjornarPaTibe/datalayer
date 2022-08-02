4a9a56e068920db261ba2f37b0e4f6917832586e
BjornarPaTibe <109271236+BjornarPaTibe@users.noreply.github.com>
2022-08-02 09:42:33 +0200

    Create events_datalayer.js

---
+++ events_datalayer.js
@@ -0,0 +1,31 @@
+window.dataLayer = window.dataLayer || []
+window.dataLayer.push({
+   'event': 'Sendt inn innbytteskjema',
+  //Get values from database
+  'produktnavn':'Dethleffs Trend T 7017',
+  'pris': 839000,
+  'salgstype':'brukt',
+  'kilometeravstand': '12000km',
+  'girkasse':'Manuell',
+  'hestekrefter':149,
+  'antall_seter':4,
+  'årsmodell':'2017',
+  'totalvekt': '3850kg',
+  'egenvekt':'3043kg'
+});
+
+window.dataLayer = window.dataLayer || []
+window.dataLayer.push({
+   'event': 'Sendt inn skjema - Produkt',
+  //Get values from database
+  'produktnavn':'Dethleffs Trend T 7017',
+  'pris': 839000,
+  'salgstype':'brukt',
+  'kilometeravstand': '12000km',
+  'girkasse':'Manuell',
+  'hestekrefter':149,
+  'antall_seter':4,
+  'årsmodell':'2017',
+  'totalvekt': '3850kg',
+  'egenvekt':'3043kg'
+});
